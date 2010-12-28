aca02543310919f6675fa664a824cfe06c43a769
Adam Prendergast <prenders82@yahoo.com>
2010-12-28 11:46:14 +0000

    Complete user signup

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -45,7 +45,35 @@ describe UsersController do
   	    post :create, :user => @attr
   	    response.should render_template('new')
   	  end
-    end 
+    end
+    
+    describe "success" do
+    
+      before(:each) do
+        @attr = { :name => "Foo Bar",
+                  :email => "foobar@example.com",
+                  :password => "secret",
+                  :password_confirmation => "secret"
+        }
+      end
+    
+      it "should create a user" do
+        lambda do
+          post :create, :user => @attr
+        end.should change(User, :count).by(1)
+      end
+      
+      it "should redirect to the user show page" do
+        post :create, :user => @attr
+        response.should redirect_to(user_path(assigns(:user)))
+      end
+      
+      it "should have a welcome message" do
+        post :create, :user => @attr
+        flash[:success].should =~ /welcome to the sample app/i
+      end
+      
+    end
   end
   
   
