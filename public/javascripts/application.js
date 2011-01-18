3325e51c7a9f9a8c81e2823af9da95c70320daed
Adam Prendergast <prenders82@yahoo.com>
2011-01-18 22:26:07 +0000

    Added micropost character countdown

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,11 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+function textCounter(textarea, countdown, maxlimit)
+{
+  textareaid = document.getElementById(textarea);
+  if (textareaid.value.length > maxlimit)
+    textareaid.value = textareaid.value.substring(0, maxlimit);
+  else
+    document.getElementById(countdown).value = (maxlimit-textareaid.value.length);
+}
---
+++ public/javascripts/micropost_countdown.js
@@ -0,0 +1,8 @@
+function textCounter(textarea, countdown, maxlimit)
+{
+  textareaid = document.getElementById(textarea);
+  if (textareaid.value.length > maxlimit)
+    textareaid.value = textareaid.value.substring(0, maxlimit);
+  else
+    document.getElementById(countdown).value = '('+(maxlimit-textareaid.value.length)+' characters available)';
+}
