9446875c8cfc850566575471b089e66a8cf9491a
Adam Prendergast <prenders82@yahoo.com>
2011-01-20 14:03:57 +0000

    Complete exercise 11 exercises

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,7 +3,10 @@ require 'spec_helper'
 describe PagesController do
 	render_views
 
+### GET 'home'
+
   describe "GET 'home'" do
+   
    it "should be successful" do
       get 'home'
       response.should be_success
@@ -14,7 +17,46 @@ describe PagesController do
   		response.should have_selector("title",
   								:content => "Ruby on Rails Tutorial Sample App | Home")
   	end
+  	
+  	describe "for signed in users" do
+  	  
+  	  before(:each) do
+        @user = Factory(:user)
+        test_sign_in(@user)
+      end
+      
+      it "should have the singular display for one user micropost" do
+        mp1 = Factory(:micropost, :user => @user)
+        get 'home'
+        response.should have_selector("span.microposts", :content => "micropost")
+      end
+      
+      it "should have the plural display for many user microposts" do
+        mp1 = Factory(:micropost, :user => @user)
+        mp2 = Factory(:micropost, :user => @user)
+        get 'home'
+        response.should have_selector("span.microposts", :content => "micropost")
+      end
+      
+      it "should paginate the micropost feed" do
+        @microposts = []
+        31.times do
+          @microposts << Factory(:micropost, :user => @user)
+        end
+        get 'home'
+        response.should have_selector("div.pagination")
+        response.should have_selector("span.disabled", :content => "Previous")
+        response.should have_selector("a", :href => "/?page=2",
+                                           :content => "Next")
+        response.should have_selector("a", :href => "/?page=2",
+                                         :content => "2")
+      end
+      
+  	end
+  	
   end
+
+### GET 'contact'
 
   describe "GET 'contact'" do
     it "should be successful" do
@@ -28,6 +70,8 @@ describe PagesController do
   								:content => "Ruby on Rails Tutorial Sample App | Contact")
   	end
   end
+  
+### GET 'about'  
   
   describe "GET 'about'" do
   	it "should be successful" do
